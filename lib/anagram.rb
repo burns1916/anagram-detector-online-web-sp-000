281fd6c0e1754600f94d2247e0b0f0dae5e989cc
Ryan K Burns <ryankburns@gmail.com>
2020-10-07 21:52:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+attr_accessor :word
+
+  def initialize(word)
+    @word_split = word.split("")
+    @word_split_ordered = @word_split.sort
+    if @word_split_ordered == @word_array_ordered
+      @word
+    else
+      []
+    end
+  end
+
+  def match(word_array)
+    word_array.each do |words_in_array|
+    @words_in_array_split = words_in_array.split("")
+    @word_array_ordered = @words_in_array_split.sort
+    binding.pry
+    end
+  end
+
+end
