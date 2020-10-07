2610ab07a7b0d3666b54452efbe252f8176fd959
Ryan K Burns <ryankburns@gmail.com>
2020-10-07 22:07:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,20 +5,13 @@ class Anagram
 attr_accessor :word
 
   def initialize(word)
-    @word_split = word.split("")
-    @word_split_ordered = @word_split.sort
-    if @word_split_ordered == @word_array_ordered
-      return @match
-    else
-      []
-    end
+    @word_organized = word.split("").sort
   end
 
   def match(word_array)
     word_array.each do |words_in_array|
     @word_array_organized = words_in_array.split("").sort
-    binding.pry
-    if @word_split_ordered == @word_array_ordered
+    if @word_organized == @word_array_organized
     @word
     else
       []
