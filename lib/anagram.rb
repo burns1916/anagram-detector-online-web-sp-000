820d67d58ec9d88f083e1ac4cc9aabc45a8fde93
Ryan K Burns <ryankburns@gmail.com>
2020-10-07 22:28:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,7 +13,7 @@ attr_accessor :word
     @word_array_organized = words_in_array.split("").sort
     end
     if @word_organized == @word_array_organized
-      @word
+      word
     else
       @no_match = []
     end
