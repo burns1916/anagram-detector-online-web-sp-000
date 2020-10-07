93a31691d9abc43c8735a9e19ba3c63f9488b193
Ryan K Burns <ryankburns@gmail.com>
2020-10-07 22:39:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,8 +9,8 @@ attr_accessor :word
   end
 
   def match(word_array)
-    word_array.each do |words_in_array|
-    words_in_array.split("").sort == word.split("").sort
+    word_array.select do |words_in_array|
+    words_in_array.split("").sort == @word.split("").sort
     end
   end
 
