3cc3b502a072beaf80795cd5ca2ec8153b77df11
Ryan K Burns <ryankburns@gmail.com>
2020-10-07 22:30:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,8 +11,10 @@ attr_accessor :word
   def match(word_array)
     word_array.each do |words_in_array|
     @word_array_organized = words_in_array.split("").sort
+    @match = @word_array_organized == @word_organized
     end
-    if @word_organized == @word_array_organized
+    if @match == true
+      @match
     else
       @no_match = []
     end
