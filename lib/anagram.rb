a3be36540ba9b94be4835eb3e9520c12c6d8c0ca
Ryan K Burns <ryankburns@gmail.com>
2020-10-07 22:10:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ attr_accessor :word
     word_array.each do |words_in_array|
     @word_array_organized = words_in_array.split("").sort
     if  @word_array_organized == @word_organized
-    @word
+    return @word
     else
       []
     end
