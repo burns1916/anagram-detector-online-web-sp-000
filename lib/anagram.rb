750ea5b2d877cf171f2780cf91153d7e9db2cd3d
Ryan K Burns <ryankburns@gmail.com>
2020-10-07 22:11:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ attr_accessor :word
   def match(word_array)
     word_array.each do |words_in_array|
     @word_array_organized = words_in_array.split("").sort
-    if  @word_array_organized == @word_organized
+    if @word_array_organized == @word_organized
       return @word
     else
       @no_match = []
